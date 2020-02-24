542a0c40279845f8ed57daeafa498ae87e168f05
Jessica <jrnewport07@gmail.com>
2020-02-24 11:33:36 -0600

    assignment complete pre-bonus, list table off by one column

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,18 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping(value="results")
+    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+        ArrayList<Job> jobs;
+        if(searchType.toLowerCase().equals("all")){
+            jobs = JobData.findAll();
+        }else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
 }
